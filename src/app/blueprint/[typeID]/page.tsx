936b798172cb919a5cac7e37f3113e1c7dad9e23
Philihp Busby <philihp@gmail.com>
2024-10-23 10:13:40 +0000

    feature: show rigs that will modify a blueprint type

--- src/app/blueprint/[typeID]/page.tsx
+++ src/app/blueprint/[typeID]/page.tsx
@@ -1,4 +1,7 @@
 import { createClient } from '@/utils/supabase/server'
+import { categoriesWithFilter, filters, filtersUsed, groupsWithFilter } from './filters'
+import { filtersForRigs, rigsForFilter } from './modifiers'
+import { flatten, map } from 'ramda'
 
 type BlueprintTypeParams = {
   params: {
@@ -8,13 +11,42 @@ type BlueprintTypeParams = {
 
 const BlueprintType = async ({ params: { typeID } }: BlueprintTypeParams) => {
   const supabase = createClient()
+
   const {
-    data: { typeName, description },
-  } = await supabase.schema('evesde').from('invTypes').select().eq('typeID', typeID).single()
+    data: { productTypeID },
+  } = await supabase.schema('evesde').from('industryActivityProducts').select().eq('typeID', typeID).limit(1).single()
+
+  const {
+    data: { typeName, groupID },
+  } = await supabase.schema('evesde').from('invTypes').select().eq('typeID', productTypeID).single()
+
+  const {
+    data: { categoryID },
+  } = await supabase.schema('evesde').from('invGroups').select().eq('groupID', groupID).single()
+
+  const { data: rigs } = await supabase
+    .schema('evesde')
+    .from('invTypes')
+    .select('typeID, typeName')
+    .in('typeID', flatten(map(rigsForFilter, filtersUsed(groupID, categoryID))))
+    .order('typeName')
+
+  const sortedRigs = (rigs ?? []).sort(({ typeName: typeNameA }, { typeName: typeNameB }) => {
+    let a = typeNameA
+    let b = typeNameB
+    if (typeNameA.includes('M-Set')) a = 1
+    if (typeNameB.includes('M-Set')) b = 1
+    if (typeNameA.includes('L-Set')) a = 2
+    if (typeNameB.includes('L-Set')) b = 2
+    if (typeNameA.includes('XL-Set')) a = 3
+    if (typeNameB.includes('XL-Set')) b = 3
+    return a - b
+  })
+
   return (
     <>
       <h1>{typeName}</h1>
-      <pre>Slug: {JSON.stringify({ typeName, description }, undefined, 2)}</pre>
+      <ul>{rigs?.map(({ typeID, typeName }) => <li key={typeID}>{typeName}</li>)}</ul>
     </>
   )
 }
